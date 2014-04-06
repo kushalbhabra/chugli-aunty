4072c10e93dc6499baa565a138b1515bd9222822
manojgudi <manoj.p.gudi@gmail.com>
2014-04-07 00:45:24 +0530

    Working functions put|get Added to Aunty Aunty ready for Alpha testing Work on: > Access level restriction on put > Making code cleaner > Shift from *deprecated* BlobStore to Google Cloud Storage > Added code to return junk queries(email-subject) > CODE Cleanup

--- handle_incoming_email.py
+++ handle_incoming_email.py
@@ -2,6 +2,9 @@ import logging, email
 import wsgiref.handlers
 import exceptions
 
+
+
+from google.appengine.ext import db
 from google.appengine.api import mail
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
@@ -12,7 +15,18 @@ from google.appengine.ext import ndb
 from auntymodel import Paper
 from SCL import *
 
+class LookUp(db.Model):
+	FileName = db.StringProperty()
+	BlobKey = blobstore.BlobReferenceProperty()
+
+
 class LogSenderHandler(InboundMailHandler):
+    
+
+    def construct_name(self, query):
+        self.name = query["subject"]+"_"+query["number"]+"_"+query["exam"]+"_"+query["year"]
+        return self.name
+	
     def receive(self, mail_message):
         logging.info("================================")
         logging.info("Received a mail_message from: " + mail_message.sender)
@@ -24,13 +38,18 @@ class LogSenderHandler(InboundMailHandler):
         
         if self.Query["method"] == "get":
                 logging.info("using get method")
+                self.get(self.Query, mail_message)
+
         elif self.Query["method"] == "put":
                 logging.info("using put method")
                 self.put(self.Query, mail_message)
+
         else:
                 logging.info("something chutiya method")
         
     def put(self, query, mail_message):
+    
+        self.look_up = LookUp()
         
         if not hasattr(mail_message, 'attachments'):
             return
@@ -42,12 +61,13 @@ class LogSenderHandler(InboundMailHandler):
             filename = attach[0]
             contents = attach[1]
         
-        self.pdf_name = query["subject"]+"_"+query["number"]+"_"+query["exam"]+"_"+query["year"]
+
+	self.pdf_name = self.construct_name(query)
 
         file_name = files.blobstore.create(mime_type = "application/pdf", _blobinfo_uploaded_filename= self.pdf_name+".pdf")
            
         logging.info("PDF FILE name"+ self.pdf_name)
-        logging.info("written")
+        logging.info("written" + str(file_name))
 
         # Open the file and write to it
         with files.open(file_name, 'a') as f:
@@ -55,9 +75,52 @@ class LogSenderHandler(InboundMailHandler):
 
         # Finalize the file. Do this before attempting to read it.
         files.finalize(file_name)
+ 
+        # Get the file's blob key
+        blob_key = files.blobstore.get_blob_key(file_name)
+        
+        #### PUT the values in LookUp datastore
+        self.look_up.FileName = self.pdf_name
+        self.look_up.BlobKey = blob_key
+        self.look_up.put()
 
+    
+    def get(self, query, mail_message):
+	
+	self.name = self.construct_name(query)
+	logging.info("FILE NAME "+self.name)
 
-        '''
+        self.lookup = LookUp.all()
+        self.files_list = self.lookup.filter("FileName =", self.name)
+	
+	logging.info("Query "+ str(dir(self.files_list)))
+	
+	##  TO Add if files_list is empty then email "FILE NOT FOUND" message
+
+	for  self.pdf_file in self.files_list.run():
+		logging.info("Got PDF FILE_named " + str(self.pdf_file.FileName))
+		logging.info("PDF dir" + str(dir(self.pdf_file.BlobKey)))
+	
+		self.blob_info = blobstore.BlobInfo.get(self.pdf_file.BlobKey.key())
+        	self.blob_reader = blobstore.BlobReader(self.pdf_file.BlobKey.key())
+        
+	        self.value = self.blob_reader.read()
+                    
+		mail.send_mail(sender="chugliaunty@gmail.com",
+                	to=mail_message.sender,
+                	subject=mail_message.subject,
+                	body="Hi, I hope that you got what you were looking for, Regards, Aunty",
+                	attachments=[(self.blob_info.filename, self.value)]
+		)
+         
+		
+'''
+ 	self.database = db.Query(LookUp)
+	self.database.filter("FileName=", self.name)
+	
+	for pdf_file in self.database.filter
+
+   
         if not hasattr(mail_message, 'attachments'):
             return
             #raise ProcessingFailedError('Email had no attached documents')
