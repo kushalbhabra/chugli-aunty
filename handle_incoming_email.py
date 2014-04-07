29963fd483fcc419bc5a75f7bae155d71b704fb0
manojgudi <manoj.p.gudi@gmail.com>
2014-04-07 23:27:37 +0530

    cleanups | added email-id logger

--- handle_incoming_email.py
+++ handle_incoming_email.py
@@ -12,6 +12,7 @@ from google.appengine.ext.webapp.mail_handlers import InboundMailHandler
 from google.appengine.api import files
 from google.appengine.ext import blobstore
 from google.appengine.ext import ndb
+from EmailModel import *
 from auntymodel import Paper
 from SCL import *
 
@@ -28,6 +29,7 @@ class LogSenderHandler(InboundMailHandler):
         return self.name
 	
     def receive(self, mail_message):
+ 	self.email_db = Emaildb()
         logging.info("================================")
         logging.info("Received a mail_message from: " + mail_message.sender)
         logging.info("my email subject " + mail_message.subject)
@@ -50,7 +52,7 @@ class LogSenderHandler(InboundMailHandler):
     def put(self, query, mail_message):
     
         self.look_up = LookUp()
-        
+
         if not hasattr(mail_message, 'attachments'):
             return
         else:
@@ -83,8 +85,13 @@ class LogSenderHandler(InboundMailHandler):
         self.look_up.FileName = self.pdf_name
         self.look_up.BlobKey = blob_key
         self.look_up.put()
+	
 
-    
+        #### PUT the values in EmailDB datastore
+    	self.email_db.subject = mail_message.subject
+	self.email_db.emailer = mail_message.sender
+	self.email_db.put()
+
     def get(self, query, mail_message):
 	
 	self.name = self.construct_name(query)
@@ -109,89 +116,16 @@ class LogSenderHandler(InboundMailHandler):
 		mail.send_mail(sender="chugliaunty@gmail.com",
                 	to=mail_message.sender,
                 	subject=mail_message.subject,
-                	body="Hi, I hope that you got what you were looking for, Regards, Aunty",
+                	body="Hi, I hope you got what you were looking for, Regards, Aunty",
                 	attachments=[(self.blob_info.filename, self.value)]
 		)
-         
-		
-'''
- 	self.database = db.Query(LookUp)
-	self.database.filter("FileName=", self.name)
-	
-	for pdf_file in self.database.filter
+ 
+        #### PUT the values in EmailDB datastore
+    	self.email_db.subject = mail_message.subject
+	self.email_db.emailer = mail_message.sender
+	self.email_db.put()
 
-   
-        if not hasattr(mail_message, 'attachments'):
-            return
-            #raise ProcessingFailedError('Email had no attached documents')
-        else:
-            
-            logging.info("Email has %i attachment(s) " % len(mail_message.attachments))
-            
-        filname=''
-
-        for attach in mail_message.attachments:
-            filename = attach[0]
-            contents = attach[1]
-
-
-        # Create the file
-        file_name = files.blobstore.create(mime_type = "application/pdf", _blobinfo_uploaded_filename='filename.pdf')
-
-        # Open the file and write to it
-        with files.open(file_name, 'a') as f:
-            f.write(contents.decode())
-
-        # Finalize the file. Do this before attempting to read it.
-        files.finalize(file_name)
-
-        # Get the file's blob key
-        blob_key = files.blobstore.get_blob_key(file_name)
-
-        paper = Paper()
-        paper.subject = mail_message.subject
-        paper.uploaded_by = mail_message.sender
-        paper.blob_key = blob_key
-        paper.put()
         
-        try:
-            logging.info( "BLOB KEY:" )
-            logging.info( blob_key )
-        except:
-            logging.info("ERROR LOGGING BLOB KEY")
-
-        blob_info = blobstore.BlobInfo.get(blob_key)
-        blob_reader = blobstore.BlobReader(blob_key)
-    
-        value = blob_reader.read()
-                
-        mail.send_mail(sender="chugliaunty@gmail.com",
-              to=mail_message.sender,
-              subject=mail_message.subject,
-              body="",
-              attachments=[(blob_info.filename, value)]
-        )
-        
-        papers = Paper.query(Paper.subject == mail_message.subject)
-        for p in papers:                
-            try:
-                logging.info( "BLOB KEY:" )
-                logging.info( p.blob_key )
-            except:
-                logging.info("ERROR LOGGING BLOB KEY")
-
-            blob_info = blobstore.BlobInfo.get(p.blob_key)
-            blob_reader = blobstore.BlobReader(p.blob_key)
-        
-            value = blob_reader.read()
-                    
-            mail.send_mail(sender="chugliaunty@gmail.com",
-                  to=mail_message.sender,
-                  subject=mail_message.subject,
-                  body="Hi, <br />PFA. <br /><br /> Regards, Aunty",
-                  attachments=[(p.subject, value)]
-            )
-           '''                                                
 
 app = webapp.WSGIApplication([LogSenderHandler.mapping()], debug=True)
 wsgiref.handlers.CGIHandler().run(app)
