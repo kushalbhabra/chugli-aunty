f4cee8753812d4c92a03bb73f039722c616fae0a
manojgudi <manoj.p.gudi@gmail.com>
2014-04-10 12:23:21 +0530

    added a basic readme

--- chugliaunty.py
+++ chugliaunty.py
@@ -38,7 +38,37 @@ class MainHandler(webapp2.RequestHandler):
     upload_url = blobstore.create_upload_url('/upload')
     self.response.out.write('<html><body>')
     self.response.out.write('<center><h2>Chugli Aunty ; )</h2>')
-    self.response.out.write('<p>get and put email here: <strong>help@chugliaunty.appspotmail.com</strong></p></center>')
+    self.response.out.write('<p>get and email here: <strong>help@chugliaunty.appspotmail.com</strong></p></center>')
+    
+    # Somebody write this using template
+    self.response.out.write("<h2>FAQ Section</h2> <br><h3> Who's/What's Chugli Aunty?</h3>  \
+    Chugli Aunty is an auto-responding email bot for all people who renew their friendship with Maggus in their class \
+    <b>just before</b> the exam time so that they get access to all past papers.<br> \
+    Now no more doing that torturous stuff, get instant access to all uploaded past papers delievered \
+    directly to your email-account. Just write a relevant SCL query in your email subject, hit send,\
+    and curse your maggu friend (for now you have neatly arranged emails containing past-papers!).<br><br<br>\
+													\
+    <h3>SQL suna hai, what the fk is SCL?</h3>\
+    Good question; Chugli aunty requires chugli to process information and send you relevant papers. So, if SQL stands\
+    for Structured Query Language, SCL stands for <b>Structured Chugli Language</b>. Just write SCL query in your email-subject, and that's it!<br> \
+    An example of SCL to get 2013 Design Lab CL455 endsem paper of chemical dept:<br>\
+    <i> get cl 455 endsem 13</i> <br><br>\
+						\
+    you can use asterisks to get papers of all years and all exam of a particular course<br>\
+    <i> get cl455 * * </i> \
+    				<br>\
+    Similary you can use <b>list</b> command in place \"get\" to get a list of relevant papers.\
+    <br><br>\
+					\
+    <h3>Nice! Who made this? Why?</h3>\
+    Do you have time to read this section? Maggna shuru karo.. <br><br><br>\
+    There is good probability that a <b>neckless fatty</b> called Aditya Patil will claim that he wrote/co-wrote this software.<br>\
+    That IS 101.73% false! <br> IF he could code, then you wouldn't have seen this.\
+	<br><br>\
+	\"Software is like sex: it's better when it's free\"-Linus Torvalds<br>\
+	<i>we completely agree:</i> <a href='https://github.com/kushalbhabra/chugli-aunty'>source</a> ")
+
+
     self.response.out.write('</body></html>')
     '''
     self.response.out.write('<html><body>')
