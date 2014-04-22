21a9b4d1567a4fa82799aae2d0aa90e368274deb
manojgudi <manoj.p.gudi@gmail.com>
2014-04-22 11:26:30 +0530

    trivial change

--- handle_incoming_email.py
+++ handle_incoming_email.py
@@ -219,7 +219,7 @@ class LogSenderHandler(InboundMailHandler):
 		to=mail_message.sender,
 		subject="Nahi Chamka!",
 		body="Sorry! could'nt understand Chuglimail with *Subject*: %s. You can ALSO use asterisks in your chugli, keep SPACES inbetween,  [BETA Auto-suggestion Feature]: Copy-paste this as your subject: %s                              CHECK http://chugliaunty.appspot.com on how to construct query,              Regards, Aunty"  %(str(mail_message.subject), str("get "+ query['subject']+' '+query['number']+' '+"*"+' '+"*" )),
-		html="Sorry! could'nt understand Chuglimail with <b>Subject</b>: %s. <br><br>You can ALSO use asterisks in your chugli, keep SPACES inbetween, <br><br>[BETA Auto-correction Feature]: <b>Copy-paste this as your subject: %s </b> <br><br> CHECK http://chugliaunty.appspot.com on how to construct query, <br><br> Regards, Aunty"  %(str(mail_message.subject), str("get "+ query['subject']+' '+query['number']+' '+"*"+' '+"*" ))
+		html="Sorry! could'nt understand Chuglimail with <b>Subject</b>: %s. <br><br>You can ALSO use asterisks in your chugli, keep SPACES inbetween, <br><br>[BETA Auto-suggestion Feature]: <b>Copy-paste this as your subject: %s </b> <br><br> CHECK http://chugliaunty.appspot.com on how to construct query, <br><br> Regards, Aunty"  %(str(mail_message.subject), str("get "+ query['subject']+' '+query['number']+' '+"*"+' '+"*" ))
 	)
 	
     # When no files are found, mail relevant message.
